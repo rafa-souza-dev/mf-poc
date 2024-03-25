a95d5a764242fe94d9102c88f2ac42450118ee78
Rafael Souza <rafael.jose@ferreri.co>
2024-03-25 14:28:44 -0300

    update _document file

--- app1/src/pages/_document.tsx
+++ app1/src/pages/_document.tsx
@@ -1,39 +1,19 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 import React from "react";
-import { revalidate, FlushedChunks, flushChunks } from "@module-federation/nextjs-mf/utils";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    if (process.env.NODE_ENV === "development" && !ctx?.req?.url?.includes("_next")) {
-      await revalidate();
-    } else {
-      ctx?.res?.on("finish", () => {
-        revalidate();
-      });
-    }
-
     const initialProps = await Document.getInitialProps(ctx);
-    const chunks = await flushChunks();
-  
-    return {
-      ...initialProps,
-      chunks,
-    };
+    return { ...initialProps };
   }
 
   render() {
-    const chunks = this.props.__NEXT_DATA__.props.chunks || [];
-
     return (
       <Html>
-        <Head>
-          <meta name="robots" content="noindex" />
-          <FlushedChunks chunks={chunks} />
-        </Head>
-
-        <body className="bg-background-grey">
-        <Main />
-        <NextScript />
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
--- app2/src/pages/_document.tsx
+++ app2/src/pages/_document.tsx
@@ -1,39 +1,28 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 import React from "react";
-import { revalidate, FlushedChunks, flushChunks } from "@module-federation/nextjs-mf/utils";
+import { revalidate } from "@module-federation/nextjs-mf/utils";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    if (process.env.NODE_ENV === "development" && !ctx?.req?.url?.includes("_next")) {
-      await revalidate();
-    } else {
-      ctx?.res?.on("finish", () => {
-        revalidate();
-      });
-    }
+    await revalidate();
 
     const initialProps = await Document.getInitialProps(ctx);
-    const chunks = await flushChunks();
   
     return {
       ...initialProps,
-      chunks,
     };
   }
 
   render() {
-    const chunks = this.props.__NEXT_DATA__.props.chunks || [];
-
     return (
       <Html>
         <Head>
           <meta name="robots" content="noindex" />
-          <FlushedChunks chunks={chunks} />
         </Head>
 
         <body className="bg-background-grey">
-        <Main />
-        <NextScript />
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
