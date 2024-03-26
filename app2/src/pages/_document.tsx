8eb2c1d35c777eebe126b365d2c6781ab91158c2
Rafael Souza <rafael.jose@ferreri.co>
2024-03-25 23:42:33 -0300

    apply commom document

--- app2/src/pages/_document.tsx
+++ app2/src/pages/_document.tsx
@@ -1,26 +1,17 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 import React from "react";
-import { revalidate } from "@module-federation/nextjs-mf/utils";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    await revalidate();
-
     const initialProps = await Document.getInitialProps(ctx);
-  
-    return {
-      ...initialProps,
-    };
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html>
-        <Head>
-          <meta name="robots" content="noindex" />
-        </Head>
-
-        <body className="bg-background-grey">
+        <Head />
+        <body>
           <Main />
           <NextScript />
         </body>
